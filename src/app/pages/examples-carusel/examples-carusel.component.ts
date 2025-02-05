9f5a994125eac08a2b06b0d1e31fdd1943df56b8
Mr. Otabek <meliqoziyevotabek66@gmail.com>
2025-02-05 16:20:47 +0500

    first commit

---
+++ src/app/pages/examples-carusel/examples-carusel.component.ts
@@ -0,0 +1,10 @@
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-examples-carusel',
+  templateUrl: './examples-carusel.component.html',
+  styleUrls: ['./examples-carusel.component.scss']
+})
+export class ExamplesCaruselComponent{
+  
+}
